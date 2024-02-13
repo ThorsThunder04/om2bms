19c952cd7117d957c30f6f68edd23cecb227a1da
sanzelda <87436831+sanzelda@users.noreply.github.com>
2024-02-14 00:05:30 +0100

    Main conversion function + terminal UI
    - Creation of a function named `convert()` in `om2bms_osz.py` to do the same thing as calling the file from the command line, but through function arguments.
    - Creation of a command line interface for `.osz` conversion that provides a simpler user interface. It also incorporates batch conversion for all files inside a given input file.
    - Updated README with the above and other stuff relevant to the project.

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # o!m2bms
 
-A 7k/8k osu!mania to BMS file converter.
+[Original Repo](https://github.com/mashimycota/om2bms)
+
+Program that converts osu!mania maps to BMS maps. (**currently only supports 7k and 8k**)
 
 Supports fully keysounded maps and bpm changes.
 
-[Original Repo](https://github.com/mashimycota/om2bms)
+**If you come across any Errors, please report them in the issues!**
 
 ### Installation
 
-Written on Python 3.6+ and requires Pillow (for image resizing).
+Written on Python 3.6+.
+You can download the required libraries by running:
+```
+pip install -r requirements.txt
+```
 
-```
-pip install Pillow
-```
+Requires an instance of ffmpeg in your system PATH. Here's an [ffmpeg installtion tutorial](https://phoenixnap.com/kb/ffmpeg-windows) if you don't have it already. (To check it if works, type `ffmpeg` in your terminal, and it should not return an error)
+
 
 Set the default output directory by running,
 
@@ -34,34 +39,47 @@ to convert all 7k/8k files in `sample_osz_file.osz` and output them to `[OUTPUT 
 To convert individual .osu files run
 
 ```
-python om2bms.py -i sample_osu_file.osu
+python om2bms_osu.py -i sample_osu_file.osu
 ```
 
+#### Batch Converting
+
+Run
+```
+python commandUI.py
+```
+To access the Command Line User Interface that allows some settings customisation and batch conversion of `.osz` files that are in the input folder (not compatible with single `.osu` files for now) 
+
+(I personally think always converting `.osz` files is best anyway)
 
 
-To view help, run
+### To view help, run
 
 ```
 python om2bms_osz.py -h
 ```
 
 ```
-python om2bms.py -h
+python om2bms_osu.py -h
 ```
 
 
 
 ### To-do List
 
-- Drag-and-drop GUI
-- 5k maps
-- Key options for non-7k/8k maps
+- [ ] Slider Velocity
+- [ ] Difficulty calculation when converting 
+- [ ] Background Videos
+- [ ] Support other keycounds then just 7k and 8k
+- [ ] Improved background image resize/processing when converting
+- [x] Adapt to be used without command line, aka a general run function for the arguments (necessary for GUI and improving command line UI)
+- [ ] Make GUI
+- [ ] Drag-and-drop for GUI
 
 ### Notes
 
-- There may be a global ~-5ms offset (Use "-o" option to adjust offset)
 - Tested with LR2
-- Does not support SV changes
+- Does not currently support SV changes
 - Uses first timing point to estimate the first measure
 - Assumes timing points are always the start of a new measure
 - If there are >1 hitsounds played in one note, only plays the hitsound with the highest bit
--- om2bms_osz.py
+++ om2bms_osz.py
@@ -5,10 +5,118 @@ import shutil
 from argparse import ArgumentParser
 from om2bms.exceptions import BMSMaxMeasuresException
 from om2bms.extra_file_processing import size_bg_thumbnail, cut_save_preview_audio
+from types import SimpleNamespace
 
 import om2bms.om_to_bms
 import multiprocessing
 
+def convert(in_file: str, set_default_out: str = None, hitsound: bool = None, bg: bool = True, foldername: str = None, offset: int = -235, judge: int = 2):
+
+    args = SimpleNamespace(**{ # Allows simpler use of the paramaters
+        "in_file": in_file, "set_default_out": set_default_out,
+        "hitsound": hitsound,"bg": bg,
+        "foldername": foldername, "offset": offset,
+        "judge": judge
+    })
+
+    cwd = os.getcwd()
+
+    cfg_file = os.path.join(cwd, 'default_outdir.ini')
+    if not os.path.exists(cfg_file):
+        with open(cfg_file, "w") as file:
+            file.write("")
+    if args.set_default_out is not None and args.set_default_out:
+        with open(cfg_file, 'w') as cfg_fp:
+            cfg_fp.write(args.set_default_out)
+            # print('Default output directory has been set to "%s"' % args.set_default_out)
+            cfg_fp.close()
+        outdir = args.set_default_out.strip()
+
+    if args.in_file is None:
+        exit(0)
+
+    if os.path.exists(cfg_file):
+        with open(cfg_file, "r") as cfg_fp:
+            outdir = cfg_fp.readline().strip()
+            cfg_fp.close()
+    else:
+        outdir = cwd
+
+    if args.foldername is None:
+        foldername = os.path.basename(args.in_file)[:-4]
+        out_foldername = foldername
+    else:
+        out_foldername = args.foldername
+        
+    unzip_dir = os.path.join(cwd, "unzip_dir")
+    if not os.path.isdir(unzip_dir):
+        os.makedirs(unzip_dir)
+    try:
+        if not os.path.isdir(unzip_dir):
+            os.makedirs(unzip_dir)
+
+        filename = os.path.basename(args.in_file)
+        shutil.copy2(args.in_file, unzip_dir)
+
+        osz_dir = os.path.join(unzip_dir, filename)
+
+        base = os.path.splitext(osz_dir)[0]
+        os.rename(osz_dir, base + ".zip")
+        osz_file_dir = base + ".zip"
+
+        print("Unzipping...")
+        with zipfile.ZipFile(osz_file_dir, 'r') as zipf:
+            zipf.extractall(unzip_dir)
+
+        output_file_dir = os.path.join(outdir, out_foldername)
+        print("Output directory is " + output_file_dir)
+
+        if not os.path.isdir(output_file_dir):
+            os.makedirs(output_file_dir)
+
+        processes = []
+        manager = multiprocessing.Manager()
+        bg_list = manager.list()
+        audio_list = manager.list()
+        for file in os.listdir(unzip_dir):
+            if file.endswith(".osu"):
+                filedir = os.path.join(unzip_dir, file)
+                p = multiprocessing.Process(target=start_convertion,
+                                            args=(filedir, output_file_dir, file, args, bg_list, audio_list))
+                processes.append(p)
+
+
+        for p in processes:
+            p.start()
+
+        for p in processes:
+            p.join()
+
+        if args.bg:
+            print("Converting BG...")
+            convert_bg_list(bg_list)
+        
+        print("Snipping preview audio...")
+        make_preview_audio(audio_list)
+
+        # move files to output directory
+        for f in os.listdir(unzip_dir):
+            if not os.path.isdir(os.path.join(unzip_dir, f)):
+                if not f.split(".")[-1] == "zip" and not f.split(".")[-1] == "osu":
+                    full_path = os.path.join(unzip_dir, f)
+                    try:
+                        shutil.copy2(full_path, output_file_dir)
+                    except PermissionError as e:
+                        print(e)
+                        continue
+
+        print("Done")
+        # exit(0)
+    except Exception as e:
+        print(e)
+        exit(1)
+    finally:
+        shutil.rmtree(unzip_dir)
 
 def start_convertion(filedir_, output_file_dir_, file_, args, bg_list_, audio_list_):
     """
@@ -16,7 +124,7 @@ def start_convertion(filedir_, output_file_dir_, file_, args, bg_list_, audio_li
     """
     try:
         om2bms.om_to_bms.OsuManiaToBMSParser._convertion_options = {
-            "HITSOUND": args.hitsound,
+            "HITSOUND": None if args.hitsound == False else args.hitsound,
             "BG": args.bg,
             "OFFSET": args.offset,
             "JUDGE": args.judge
@@ -66,7 +174,7 @@ if __name__ == "__main__":
 
     parser.add_argument('-i', '--in_file',
                         action='store',
-                        default='None',
+                        default=None,
                         help='Path to .osz to be converted.',
                         type=str)
 
@@ -104,118 +212,5 @@ if __name__ == "__main__":
                         "(3: EASY), (2: NORMAL), (1: HARD), (0: VERY HARD)")
 
     args = parser.parse_args()
-    cwd = os.getcwd()
 
-    cfg_file = os.path.join(cwd, 'default_outdir.ini')
-    if not os.path.exists(cfg_file):
-        with open(cfg_file, "w") as file:
-            file.write("")
-    if args.set_default_out is not None:
-        with open(cfg_file, 'r+') as cfg_fp:
-            cfg_fp.write(args.set_default_out)
-            print('Default output directory has been set to "%s"' % args.set_default_out)
-            cfg_fp.close()
-        outdir = args.set_default_out.strip()
-
-    if args.in_file is None:
-        exit(0)
-
-    if os.path.exists(cfg_file):
-        with open(cfg_file, "r") as cfg_fp:
-            outdir = cfg_fp.readline().strip()
-            cfg_fp.close()
-    else:
-        outdir = cwd
-
-    if args.foldername is None:
-        foldername = os.path.basename(args.in_file)[:-4]
-        out_foldername = foldername
-    else:
-        out_foldername = args.foldername
-        
-    unzip_dir = os.path.join(cwd, "unzip_dir")
-    if not os.path.isdir(unzip_dir):
-        os.makedirs(unzip_dir)
-    try:
-        if not os.path.isdir(unzip_dir):
-            os.makedirs(unzip_dir)
-
-        filename = os.path.basename(args.in_file)
-        shutil.copy2(args.in_file, unzip_dir)
-
-        osz_dir = os.path.join(unzip_dir, filename)
-
-        base = os.path.splitext(osz_dir)[0]
-        os.rename(osz_dir, base + ".zip")
-        osz_file_dir = base + ".zip"
-
-        print("Unzipping...")
-        with zipfile.ZipFile(osz_file_dir, 'r') as zipf:
-            zipf.extractall(unzip_dir)
-
-        output_file_dir = os.path.join(outdir, out_foldername)
-        print("Output directory is " + output_file_dir)
-
-        if not os.path.isdir(output_file_dir):
-            os.makedirs(output_file_dir)
-
-        # convert beatmap
-        # bg_list = []
-        # for file in os.listdir(unzip_dir):
-        #     if file.endswith(".osu"):
-        #         filedir = os.path.join(unzip_dir, file)
-        #         try:
-        #             convert = om2bms.om_to_bms.OsuManiaToBMSParser(filedir, output_file_dir, file)
-        #             bg_list.append(convert.get_bg())
-        #         except BMSMaxMeasuresException as e:
-        #             print(e)
-        #             continue
-        processes = []
-        manager = multiprocessing.Manager()
-        bg_list = manager.list()
-        audio_list = manager.list()
-        for file in os.listdir(unzip_dir):
-            if file.endswith(".osu"):
-                filedir = os.path.join(unzip_dir, file)
-                p = multiprocessing.Process(target=start_convertion,
-                                            args=(filedir, output_file_dir, file, args, bg_list, audio_list))
-                processes.append(p)
-
-        for p in processes:
-            p.start()
-
-        for p in processes:
-            p.join()
-
-        # convert bg
-        # if args.hitsound:
-        #     seen = []
-        #     for bg in bg_list:
-        #         if bg is not None and bg not in seen:
-        #             black_background_thumbnail(bg)
-        #             seen.append(bg)
-        if args.bg:
-            print("Converting BG...")
-            convert_bg_list(bg_list)
-        
-        print("Snipping preview audio...")
-        make_preview_audio(audio_list)
-
-        # move files to output directory
-        for f in os.listdir(unzip_dir):
-            if not os.path.isdir(os.path.join(unzip_dir, f)):
-                if not f.split(".")[-1] == "zip" and not f.split(".")[-1] == "osu":
-                    full_path = os.path.join(unzip_dir, f)
-                    try:
-                        shutil.copy2(full_path, output_file_dir)
-                    except PermissionError as e:
-                        print(e)
-                        continue
-
-        print("Done")
-        exit(0)
-    except Exception as e:
-        print(e)
-        exit(1)
-    finally:
-        shutil.rmtree(unzip_dir)
+    convert(**vars(args)) # because this function takes actual arguments instead of kwargs
